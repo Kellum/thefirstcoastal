8f08ccd8acfb010240fe22fd70104f8b168eb3eb
Ryan Kellum <kellum@me.com>
2026-01-05 15:09:17 -0500

    Add www to non-www redirect for consistent URLs and OG metadata

--- next.config.js
+++ next.config.js
@@ -10,6 +10,21 @@ const nextConfig = {
   },
   // Use modern JavaScript output
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.thefirstcoastal.com',
+          },
+        ],
+        destination: 'https://thefirstcoastal.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
